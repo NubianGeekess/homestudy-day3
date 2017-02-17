eddcbab36d05e65a2669b528feca418cc45d0e59
Ibitoye Yussuph <ibitoyeolawale@gmail.com>
2017-02-17 14:26:55 +0100

    Corrected bugs in the while loop & added some var to object result

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
 Firstly, you are to create three functions, as prototypes to the Array
 class to return the following:
@@ -16,68 +17,95 @@ Each time you iterate, you should increase the count, to test how
 efficient your implementation is
 */
 
-Array.prototype.toTwenty = function() {
-	const array = [];
-	var i = 1;
+(function() {
 
-	for (; i <= 20; i++) {
-		array.push(i);
-	}
+	Array.prototype.toTwenty = function() {
+		let arr = [];
+		let i = 1;
 
-	return array;
-}
-
-Array.prototype.toForty = function() {
-	const array = [];
-	var i = 2;
-
-	for (; i <= 40; i += 2) {
-		array.push(i);
-	}
-
-	return array;
-}
-
-Array.prototype.toOneThousand = function() {
-	const array = [];
-	var i = 10;
-
-	for (; i <= 1000; i += 10) {
-		array.push(i);
-	}
-
-	return array;
-}
-
-Array.prototype.search = function(search) {
-	var count = 0,
-		min = 0, //minimum index
-		max = this.length - 1, //maximum index
-		index = -1;
-
-	while(min <= max) {
-		average = Math.trunc((min + max) / 2);
-
-		if (this[min] === search) {
-			index = min;
-			break;
-		} else if (this[max] === search) {
-			index = max;
-			break;
-		} else if (search < (max /4)) {
-			max = 
-		}
-		} else if (this[average] < search) {
-			min = average + 1;
-		} else if (this[average] > search) {
-			max = average - 1;
-		} else {
-			index = average;
-			break;
+		for (; i <= 20; i++) {
+			arr.push(i);
 		}
 
-		count++;
+		return arr;
 	}
 
-	return {"count": count, "index": index, "length": this.length};
-}
+	Array.prototype.toForty = function() {
+		let arr = [];
+		let i = 2;
+
+		for (; i <= 40; i += 2) {
+			arr.push(i);
+		}
+
+		return arr;
+	}
+
+	Array.prototype.toOneThousand = function() {
+		let arr = [];
+		let i = 10;
+
+		for (; i <= 1000; i += 10) {
+			arr.push(i);
+		}
+
+		return arr;
+	}
+
+
+	Array.prototype.search = function(searchItem) {
+		var result = {
+			count: 0,
+			length: this.length,
+			index: -1
+		}
+		var average,
+			min = 0, //minimum index
+			max = this.length - 1; //maximum index
+
+		/* Run this while loop as long as Minimum 
+		index is less than last index of the array
+		*/
+		while (min < max) {
+			average = Math.floor((min + max) / 2);
+
+			/* Check if Search Number is same as
+			current minimum array element
+			*/
+			if (this[min] === searchItem) {
+				result.index = min;
+				break;
+			/* Check if Search Number is same as
+			current maximum array element
+			*/
+			} else if (this[max] === searchItem) {
+				result.index = max;
+				break;
+			/* Check if Search Number is same as
+			current middle element in the array
+			*/
+			} else if (this[average] === searchItem) {
+				result.index = average;
+				break;
+			/* Check and alter the maximum and minimum array
+			if Search Number is less than current middle
+			element in the array
+			*/
+			} else if (this[average] > searchItem) {
+				min += 1;
+				max = average - 1;
+			/* Check and alter the maximum and minimum array
+			if Search Number is greater than current middle
+			element in the array
+			*/
+			} else if (this[average] < searchItem) {
+				max -= 1;
+				min = average + 1;
+			}
+			// Increase Iteration number by 1
+			result.count++;
+		}
+
+		return result;
+	}
+})()
